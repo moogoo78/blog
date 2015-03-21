1e0c2ff735fcb35171f76cfefe251742b558ead1
MooGoo <moogoo78@gmail.com>
2015-03-21 13:58:48 +0800

    fixed disqus_url cannot use relative url

--- publishconf.py
+++ publishconf.py
@@ -25,7 +25,5 @@ GOOGLE_ANALYTICS = "UA-38928548-1"
 
 EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
 
-RELATIVE_URLS = True
-
 # for templates
 GITHUB_URL = 'http://github.com/moogoo78'
